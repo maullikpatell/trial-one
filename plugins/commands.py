4d7ff4bd07794fa7bfa5669be4d306c421888b57
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-10-07 01:55:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,9 +49,8 @@ async def start(c:Client, m:Message):
 async def help_command(c, m: Message):
     s = HELP_MESSAGE.format(
                 firstname=temp.FIRST_NAME,
-                username=temp.BOT_USERNAME,
-                repo=SOURCE_CODE,
-                owner="@ask_admin001" )
+                username=temp.BOT_USERNAME,)
+                
 
     if WELCOME_IMAGE:
         return await m.reply_photo(photo=WELCOME_IMAGE, caption=s, reply_markup=HELP_REPLY_MARKUP)
