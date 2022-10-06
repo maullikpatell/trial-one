789aa5441caf35bc6f570cff40b28ee38c577df4
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-10-07 01:29:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -25,7 +25,16 @@ avl_web1 = "".join(f"- {i}\n" for i in avl_web)
 @Client.on_message(filters.command('start') & filters.private & filters.incoming)
 @private_use
 async def start(c:Client, m:Message):
-    NEW_USER_REPLY_MARKUP = [
+     WELCOME_IMAGE:
+        return await m.reply_photo(photo=WELCOME_IMAGE, caption=t, reply_markup=START_MESSAGE_REPLY_MARKUP)
+    await m.reply_text(t, reply_markup=START_MESSAGE_REPLY_MARKUP, disable_web_page_preview=True)
+
+
+    if not is_user and LOG_CHANNEL: await c.send_message(LOG_CHANNEL, f"#NewUser\n\nUser ID: `{m.from_user.id}`\nName: {m.from_user.mention}", reply_markup=reply_markup)
+    new_user = await get_user(m.from_user.id)  
+    t = START_MESSAGE.format(m.from_user.mention, new_user["method"], new_user["base_site=atglinks.com"])
+
+    if NEW_USER_REPLY_MARKUP = [
                 [
                     InlineKeyboardButton('Ban', callback_data=f'ban#{m.from_user.id}'),
                     InlineKeyboardButton('Close', callback_data='delete'),
@@ -34,29 +43,24 @@ async def start(c:Client, m:Message):
     is_user = await is_user_exist(m.from_user.id)
 
     reply_markup = InlineKeyboardMarkup(NEW_USER_REPLY_MARKUP)
-
-    if not is_user and LOG_CHANNEL: await c.send_message(LOG_CHANNEL, f"#NewUser\n\nUser ID: `{m.from_user.id}`\nName: {m.from_user.mention}", reply_markup=reply_markup)
-    new_user = await get_user(m.from_user.id)  
-    t = START_MESSAGE.format(m.from_user.mention, new_user["method"], new_user["base_site=atglinks.com"])
-
-    if WELCOME_IMAGE:
-        return await m.reply_photo(photo=WELCOME_IMAGE, caption=t, reply_markup=START_MESSAGE_REPLY_MARKUP)
-    await m.reply_text(t, reply_markup=START_MESSAGE_REPLY_MARKUP, disable_web_page_preview=True)
-
-
+    
+    
 @Client.on_message(filters.command('help') & filters.private)
 @private_use
 async def help_command(c, m: Message):
-    s = HELP_MESSAGE.format(
+  
+    WELCOME_IMAGE:
+        return await m.reply_photo(photo=WELCOME_IMAGE, caption=s, reply_markup=HELP_REPLY_MARKUP)
+    await m.reply_text(s, reply_markup=HELP_REPLY_MARKUP, disable_web_page_preview=True)
+
+  
+    if s = HELP_MESSAGE.format(
                 firstname=temp.FIRST_NAME,
                 username=temp.BOT_USERNAME)
              
                  
 
-    if WELCOME_IMAGE:
-        return await m.reply_photo(photo=WELCOME_IMAGE, caption=s, reply_markup=HELP_REPLY_MARKUP)
-    await m.reply_text(s, reply_markup=HELP_REPLY_MARKUP, disable_web_page_preview=True)
-
+    
 
 @Client.on_message(filters.command('about'))
 @private_use
