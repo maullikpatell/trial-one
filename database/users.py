db0964a70b04af2bb7e0dd95678f1ff1029c0f55
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-11-01 16:12:49 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -14,7 +14,7 @@ async def get_user(user_id):
             "user_id": user_id,
             "method":"shortener",
             "shortener_api": "acccdf4778c9453ea9f193655bde0af2af01cb9e",
-            "mdisk_api": None,
+            "mdisk_api": "QDysd2c4Dy22TBvjVIvS",
             "header_text": "",
             "footer_text": "",
             "username": None,
