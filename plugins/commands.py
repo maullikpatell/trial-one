8fb5fcef740501aa942bf47f2d9f161084655226
Maulik Patel <69646201+Maulikpatell@users.noreply.github.com>
2022-11-01 21:53:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,25 +240,6 @@ async def banner_image_handler(bot, m: Message):
             else:
                 return await m.reply_text("Image URL is Invalid")
 
-@Client.on_message(filters.command('base_site') & filters.private)
-@private_use
-async def base_site_handler(bot, m:Message):
-    user_id = m.from_user.id
-    user = await get_user(user_id)
-    cmd = m.command
-    site = user['base_site']
-    text = f"`/base_site (base_site)`\n\nCurrent base site: {site}\n\n EX: `/base_site shareus.in`\n\nAvailable base sites:\n{avl_web1}\nAnd All alternate sites to droplink.co"
-    if len(cmd) == 1:
-        return await m.reply(
-        text=text,
-        disable_web_page_preview=True)
-    elif len(cmd) == 2:    
-        base_site = cmd[1].strip()
-        if not domain(base_site):
-            return await m.reply(text=text,disable_web_page_preview=True)
-        await update_user_info(user_id, {"base_site": base_site})
-        await m.reply("Base Site updated successfully")
-
 
 @Client.on_message(filters.command('me') & filters.private)
 @private_use
