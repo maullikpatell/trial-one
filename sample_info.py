299311e20673e648ea04f4884cc0f417d3a6c753
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-07 23:55:46 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -16,3 +16,21 @@ WELCOME_IMAGE = 'https://media.istockphoto.com/photos/hello-and-welcome-written-
 LINK_BYPASS = False
 HEROKU_API_KEY = 'dvetw-rde64b-2gsyww-ewef'
 HEROKU_APP_NAME = 'MyHerokuApp'
+
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
