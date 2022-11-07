531fbf7a2cc5d1072317e75b99c91d9666da5213
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-07 23:12:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,19 +3,28 @@ import datetime
 import logging
 import logging.config
 import sys
-
-from pyrogram import Client
+from pyrogram import Client, __version__
 from pyrogram.errors.exceptions.not_acceptable_406 import ChannelPrivate
-
 from config import *
 from database import db
 from database.users import filter_users
 from helpers import ping_server, temp
 from utils import broadcast_admins
 
+from pyrogram.raw.all import layer
+from database.ia_filterdb import Media
+from database.users_chats_db import db
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
 
 
 if REPLIT:
@@ -55,42 +64,73 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
         "shortener",
+        name=SESSION,
         api_id=API_ID,
         api_hash=API_HASH,
         bot_token=BOT_TOKEN,
+        workers=50,
+        plugins={"root": "plugins"},
+        sleep_threshold=5,
         plugins=dict(root="plugins")
         )
-
-    async def start(self):
-        if REPLIT:
-            await keep_alive()
-            asyncio.create_task(ping_server())
-        temp.START_TIME = datetime.datetime.now()
-        await super().start()
         
-        if UPDATE_CHANNEL:
-            try:
-                self.invite_link = await self.create_chat_invite_link(UPDATE_CHANNEL)
-            except Exception:
-                logging.error(f"Make sure to make the bot in your update channel {UPDATE_CHANNEL}")
-                sys.exit(1)
-
+    async def start(self):
+        b_users, b_chats = await db.get_banned()
+        temp.BANNED_USERS = b_users
+        temp.BANNED_CHATS = b_chats
+        await super().start()
+        await Media.ensure_indexes()
         me = await self.get_me()
-        self.owner = await self.get_users(int(OWNER_ID))
-        self.username = f'@{me.username}'
-        temp.BOT_USERNAME = me.username
-        temp.FIRST_NAME = me.first_name
-        if not await db.get_bot_stats():
-            await db.create_stats()
-        banned_users = await filter_users({"banned": True})
-        async for user in banned_users:
-            temp.BANNED_USERS.append(user["user_id"])
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
-        await broadcast_admins(self, '** Bot started successfully **')
-        logging.info('Bot started')
 
     async def stop(self, *args):
-        await broadcast_admins(self, '** Bot Stopped Bye **')
         await super().stop()
-        logging.info('Bot Stopped Bye')
+        logging.info("Bot stopped. Bye.")
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                for message in app.iter_messages("pyrogram", 1, 15000):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
+
+app = Bot()
+app.run()
