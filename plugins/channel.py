e6e99221cb17199c140226ead7cdd4fb2e0272aa
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-09 23:15:22 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,24 +1,20 @@
-import logging
-
-from config import CHANNEL_ID, CHANNELS, OWNER_ID
-from database.users import get_user
 from pyrogram import Client, filters
-from utils import main_convertor_handler, update_stats, user_api_check
+from info import CHANNELS
+from database.ia_filterdb import save_file
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+media_filter = filters.document | filters.video | filters.audio
 
-# Channel
-@Client.on_message(~filters.forwarded & filters.chat(CHANNEL_ID) & (filters.channel | filters.group) & filters.incoming & ~filters.private & ~filters.forwarded)
-async def channel_link_handler(c: Client, message):
-    user = await get_user(OWNER_ID)
-    user_method = user["method"]
-    vld = await user_api_check(user)
-    if vld is not True and CHANNELS:
-        return await c.send_message(OWNER_ID, f"To use me in channel...{vld}")
-    if CHANNELS:
-        try:
-            await main_convertor_handler(message, user_method, True, user=user)
-            await update_stats(message, user_method)
-        except Exception as e:
-            logger.exception(e, exc_info=True)
+
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
+async def media(bot, message):
+    """Media Handler"""
+    for file_type in ("document", "video", "audio"):
+        media = getattr(message, file_type, None)
+        if media is not None:
+            break
+    else:
+        return
+
+    media.file_type = file_type
+    media.caption = message.caption
+    await save_file(media)
