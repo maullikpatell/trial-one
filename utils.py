29a7a8d199af4d30ac454bf4454ca3be8d7f05eb
maullikpatell <115573338+maullikpatell@users.noreply.github.com>
2022-11-10 23:43:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -733,7 +733,7 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-shortz = shortzy.Shortzy{SHORTENER_API=[user("shortener_api")], "atglinks.com"}
+shortz = shortzy.Shortzy{SHORTENER_API==[user("shortener_api")], "atglinks.com"}
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
